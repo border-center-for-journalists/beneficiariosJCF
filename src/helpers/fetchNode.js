d35ccd142facceb6e4b6845ae4ebb493ed13cf20
El Sonny <sonny@spaceshiplabs.com>
2021-01-30 18:28:48 +0100

    switch to prod api

--- src/helpers/fetchNode.js
+++ src/helpers/fetchNode.js
@@ -1,6 +1,6 @@
 const fetchNode = async (node, params = {}) => {
-  const url = new URL("http://localhost:1337/" + node);
-  //const url = new URL("https://padron.herokuapp.com/" + node);
+  //const url = new URL("http://localhost:1337/" + node);
+  const url = new URL("https://padronini.herokuapp.com/" + node);
   Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
   const res = await fetch(url);
   return res.json();
