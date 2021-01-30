bc548f650caef40c3f88023160ca850ba779b6b1
El Sonny <sonny@spaceshiplabs.com>
2021-01-30 18:14:51 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,76 @@
+<script>
+	import fetchNode from './helpers/fetchNode.js';
+	let query = '';
+	let timer;
+	$: params = {nombreCompleto:{contains:query.toUpperCase()}};
+	$: beneficiarioPromise = fetchNode('beneficiario', {where:JSON.stringify(params),sort:'importe DESC'});
+
+	const debounce = v => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			query = v;
+		}, 600);
+	}
+	const formatCurrency = num => {
+		return new Intl.NumberFormat('es-MX', { style: 'currency', currency: 'MXN' }).format(num)
+	}
+</script>
+
+<main>
+	<section>
+		<h1>¿Soy Beneficiario?</h1>
+		<p>El programa Jovenes construyendo un futuro a otorgado <strong> $ 40,529,994,316 pesos </strong> a <strong>2.1 millones</strong> de jovenes entre los años 2019 y 2020. Queremos transparentar el padron de beneficiarios para prevenir fraude y corrupcion en el programa.</p>
+
+		<p>Busca tu nombre y si detectas alguna anomalia denuncia a la STPS o tambien a El Norte con el fin de documentar posibles casos de operacion fraudulenta en el programa</p>
+	</section>
+	<form>
+		<input type='text' on:keyup={({ target: { value } }) => debounce(value)} />
+	</form>
+	{#await beneficiarioPromise}
+		<p>buscando...</p>
+	{:then beneficiarios}
+	<ul>
+		{#if beneficiarios.length}
+		{#each beneficiarios as beneficiario}
+			<li>
+				<span>{beneficiario.nombreCompleto}</span>
+				<span>{beneficiario.municipio.nombreMunicipio}, {beneficiario.municipio.nombreEntidad}</span>
+				<span>{formatCurrency(beneficiario.importe)}</span>
+			</li>
+		{/each}
+		{:else}
+			<p>Sin resultados</p>
+		{/if}
+	</ul>
+	{/await}
+</main>
+
+<style>
+	main{
+		display: flex;
+		flex-direction: column;
+		max-width:840px;
+		margin:0 auto;
+		padding:0 20px;
+	}
+	section h1{
+		text-align: center;
+		font-size:3em;
+	}
+	ul{
+		list-style-type:none;
+		padding:0;
+	}
+	li{
+		display: flex;
+		flex-direction: column;
+		margin-bottom:8px;
+		border-bottom:1px solid pink;
+		padding:10px 0;
+	}
+	li span:first-child{
+		font-size:16px;
+		font-weight: bold;
+	}
+	strong{font-size:1.3em;}
+</style>
---
+++ src/helpers/fetchNode.js
@@ -0,0 +1,9 @@
+const fetchNode = async (node, params = {}) => {
+  const url = new URL("http://localhost:1337/" + node);
+  //const url = new URL("https://padron.herokuapp.com/" + node);
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+  const res = await fetch(url);
+  return res.json();
+}
+
+export default fetchNode;
